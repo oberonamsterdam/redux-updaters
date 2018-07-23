e120fd35b0538f83df31aa80e1211e4e662084ba
David Ben Kooi <david@oberon.nl>
2018-07-23 15:56:53 +0200

    updated increment, decrement and their tests

--- src/updaters/increment.ts
+++ src/updaters/increment.ts
@@ -5,13 +5,19 @@ import { isNumber } from '../services/typeService';
 /**
  * Increment a number value
  * @example
- * dispatch(increment(app.counter))
+ * dispatch(increment(app.currentPage, 5, app.lastPage))
  */
-export default (statePath: StatePath) =>
+export default (statePath: StatePath, by?: number, max?: number) => (
     updater(
         'INCREMENT',
         statePath,
-        val => val + 1,
+        val => val = (getNewVal(val, by, max)),
         isNumber,
         `Toggle: ${pathName(statePath)} is not a number`
-    );
+    )
+);
+
+const getNewVal = (val: any, by: number = 1, max: number = +Infinity) => {
+    return Math.min(max, val + by);
+
+};

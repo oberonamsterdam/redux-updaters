6356ede394745f825dcfc743b44378f4bb284faa
David Ben Kooi <david@oberon.nl>
2018-08-06 11:02:28 +0200

    updated references and api documentation

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { default as createStatePaths } from './createStatePaths';
 export { default as update } from './updaters/update';
 export { default as toggle } from './updaters/toggle';
 export { default as arrayAdd } from './updaters/arrayAdd';
+export { default as increment } from './updaters/increment';
+export { default as decrement } from './updaters/decrement';
